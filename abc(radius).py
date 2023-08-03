4cde21bac6736d5ccee252f45585a53ccf8a1c83
Harshal7368 <141350788+Harshal7368@users.noreply.github.com>
2023-08-03 21:47:22 +0530

    created abc(radius).py
    this file is print radius

---
+++ abc(radius).py
@@ -0,0 +1,10 @@
+# radius of the circle from the user and computes area.
+import math
+
+radius = float(input("enter the radius of the circle : "))
+area = math.pi * (radius * radius)
+
+print("the area of the circle is : ",area)
+
+# enter the radius of the circle : 1.1
+# the area of the circle is :  3.8013271108436504
